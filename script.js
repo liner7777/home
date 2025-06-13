fed93f6df62a1bb680fed8243dde1b778fa50c1b
lines <158745212+liner7777@users.noreply.github.com>
2025-06-13 23:38:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.152.2/build/three.module.js';
-
+import * as THREE from 'three';
 const canvas = document.getElementById('bg');
 
 const renderer = new THREE.WebGLRenderer({ canvas });
